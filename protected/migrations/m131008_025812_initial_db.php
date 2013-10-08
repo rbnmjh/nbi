9118000d0ffd2f06786e5122a8d578fb446ad3e1
Rabin <rbnmjh@yahoo.com>
2013-10-08 12:35:02 +0545

    readded migration

--- protected/migrations/m131008_025812_initial_db.php
+++ protected/migrations/m131008_025812_initial_db.php
@@ -5,21 +5,21 @@ class m131008_025812_initial_db extends CDbMigration
 	public function up()
 	{
 		$sql="CREATE TABLE IF NOT EXISTS `album` (
-	  `id` int(11) NOT NULL AUTO_INCREMENT,
-	  `album_name` varchar(255) NOT NULL,
-	  `status` varchar(45) DEFAULT NULL,
-	  PRIMARY KEY (`id`)
-	) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-	
-	CREATE TABLE IF NOT EXISTS `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `album_name` varchar(255) NOT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `blog` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `content` text,
   `status` enum('0','1') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
-	CREATE TABLE IF NOT EXISTS `comment` (
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+ 
+CREATE TABLE IF NOT EXISTS `comment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `comment` text NOT NULL,
   `blog_id` int(11) DEFAULT NULL,
@@ -27,82 +27,130 @@ class m131008_025812_initial_db extends CDbMigration
   KEY `fk_comment_1` (`blog_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-	CREATE TABLE IF NOT EXISTS `gallery` (
+CREATE TABLE IF NOT EXISTS `contact_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `album_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_gallery_1` (`album_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-	
-	CREATE TABLE IF NOT EXISTS `media` (
+  `company_name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+CREATE TABLE IF NOT EXISTS `gallery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_name` varchar(255) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description` text,
+  `is_active` tinyint(1) DEFAULT '1',
+  `upload_date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `media` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `content` text NOT NULL,
   `status` enum('0','1') NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-	
-	CREATE TABLE IF NOT EXISTS `news` (
+
+CREATE TABLE IF NOT EXISTS `menus_wine` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `type` enum('menu','wine') NOT NULL DEFAULT 'menu',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12;
+
+CREATE TABLE IF NOT EXISTS `menu_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_title` varchar(255) NOT NULL,
+  `item_description` text,
+  `menu_id` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `update_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_menu_id` (`menu_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+CREATE TABLE IF NOT EXISTS `news` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text NOT NULL,
   `content` text NOT NULL,
   `status` enum('0','1') NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-	
-	CREATE TABLE IF NOT EXISTS `page` (
+
+CREATE TABLE IF NOT EXISTS `page` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `page` varchar(255) NOT NULL,
+  `page_title` varchar(255) DEFAULT NULL,
   `content` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-	CREATE TABLE IF NOT EXISTS `partner` (
+CREATE TABLE IF NOT EXISTS `partner` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `image` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-	CREATE TABLE IF NOT EXISTS `publication` (
+CREATE TABLE IF NOT EXISTS `publication` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `files` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-	CREATE TABLE IF NOT EXISTS `slider` (
+CREATE TABLE IF NOT EXISTS `slider` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `image_name` varchar(255) DEFAULT NULL,
   `upload_date` date NOT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
-	CREATE TABLE IF NOT EXISTS `subscription` (
+CREATE TABLE IF NOT EXISTS `subscription` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
-	CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `is_active` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
-	ALTER TABLE `comment`
+CREATE TABLE IF NOT EXISTS `wine_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `wine_title` varchar(255) NOT NULL,
+  `wine_description` varchar(255) DEFAULT NULL,
+  `wine_id` int(11) NOT NULL,
+  `glass_price` decimal(10,2) NOT NULL,
+  `bottle_price` decimal(10,2) NOT NULL,
+  `update_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_wine_id` (`wine_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+ALTER TABLE `comment`
   ADD CONSTRAINT `fk_comment_1` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE `menu_items`
+  ADD CONSTRAINT `fk_menu_id` FOREIGN KEY (`menu_id`) REFERENCES `menus_wine` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `wine_items`
+  ADD CONSTRAINT `fk_wine_id` FOREIGN KEY (`wine_id`) REFERENCES `menus_wine` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-  	ALTER TABLE `gallery`
-  ADD CONSTRAINT `fk_gallery_1` FOREIGN KEY (`album_id`) REFERENCES `album` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-	";
-
+";
 		$this->execute($sql);
 	}
 
